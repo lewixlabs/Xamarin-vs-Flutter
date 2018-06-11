58cecdebc5f3efdcfdb83f1adbfbb5ebdfa26d75
Luigino De Togni <luigino.detogni@outlook.com>
2018-06-11 13:15:33 +0200

    Xamarin project completed

---
+++ size_challenge/xamarin/size_challenge/size_challenge/size_challenge/Api.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace size_challenge
+{
+    public class Comic
+    {
+        [JsonProperty("img")]
+        public string ImageUrl { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+    }
+
+    class Api
+    {
+        readonly TimeSpan _restTimeout;
+
+        public Api(double RestTimeoutSec)
+        {
+            _restTimeout = TimeSpan.FromSeconds(RestTimeoutSec);
+        }
+
+        async public Task<Comic> HttpGet(Uri prmUri)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    client.Timeout = _restTimeout;
+                    HttpResponseMessage response = null;
+                    response = await client.GetAsync(prmUri).ConfigureAwait(false);
+
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        Debug.WriteLine(@"HttpGet Error");
+                        return null;
+                    }
+                    // verifica contenuto della risposta
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    if (string.IsNullOrEmpty(responseBody))
+                    {
+                        Debug.WriteLine(@"HttpGet Empty Response");
+                        return null;
+                    }
+
+                    return JsonConvert.DeserializeObject<Comic>(responseBody);
+                }
+            }
+            catch (TaskCanceledException exCanceled)
+            {
+                // errore di altro tipo
+                Debug.WriteLine(@"HttpGet Timeout Exception: {0}", exCanceled.Message);
+                return null;
+            }
+            catch (Exception ex)
+            {
+                // errore di altro tipo
+                Debug.WriteLine(@"HttpGet Exception: {0}", ex.Message);
+                return null;
+            }
+        }
+    }
+}
---
+++ size_challenge/xamarin/size_challenge/size_challenge/size_challenge/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace size_challenge
+{
+    public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            activityLoading.IsVisible = false;
+            btnLoadComic.Clicked += BtnLoadComic_Clicked;
+		}
+
+        async private void BtnLoadComic_Clicked(object sender, EventArgs e)
+        {
+            activityLoading.IsVisible = true;
+            var comicLoaded = await new Api(15).HttpGet(new Uri("https://xkcd.com/info.0.json"));
+            if (comicLoaded != null)
+            {
+                lblTitle.Text = comicLoaded.Title;
+                imgComic.Source = comicLoaded.ImageUrl;
+            }
+            activityLoading.IsVisible = false;
+
+        }
+    }
+
+
+}
